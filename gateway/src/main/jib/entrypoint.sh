1c8c0b531b9bd889708c5aa7d874787103d2514e
tsweets <tony.sweets@gmail.com>
2021-01-13 22:33:09 -0700

    add docker compose wait and externalize config/disco ip

--- gateway/src/main/jib/entrypoint.sh
+++ gateway/src/main/jib/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting Spring API Gateway Server"
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "org.beer30.springcloud.springcloud.SpringGatewayApplication"  "$@"
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "org.beer30.springcloud.gateway.SpringGatewayApplication"  "$@"
